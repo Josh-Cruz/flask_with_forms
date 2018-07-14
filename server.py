4da900e37c40f2e2c686cc7264f5fea586e0d15d
Josh-Cruz <josh@digitalmonestary.com>
2018-07-14 11:49:26 -0700

    finished a microsite  using a form

--- server.py
+++ server.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_templates
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
+app.secret_key = 'soopersekret'
 
-@app.route('/', method = 'POST')
+@app.route('/')
 def form():
+    if request.method == 'POST':
+        return render_template('results.html', session['name'])
+    return render_template('index.html')
 
+@app.route('/results', methods=['POST', 'GET'])
+def method_name():
+    if request.method == 'POST':
+        full_name = request.form['name']
+        dojo_location = request.form['Dojo_Location']
+        fav_lan = request.form['Favorite_Language']
+        comment = request.form['optional_comment']
+    return render_template('results.html', full_name = full_name, dojo_location = dojo_location, fav_lan = fav_lan, comment = comment)
+        
+        
 app.run(debug = True)   
