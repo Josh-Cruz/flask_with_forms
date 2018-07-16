36570ccb5ec780509ae256210e3763ccfb1852fc
Josh-Cruz <josh@digitalmonestary.com>
2018-07-16 15:36:21 -0700

    added form validations

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, flash, redirect
 app = Flask(__name__)
 app.secret_key = 'soopersekret'
 
@@ -11,10 +11,18 @@ def form():
 @app.route('/results', methods=['POST', 'GET'])
 def method_name():
     if request.method == 'POST':
+        # print(len(request.form['name']))
+        if len(request.form['name']) < 1:
+            flash("Name cannot be empty!")
+            return redirect('/')
+        if len(request.form['optional_comment']) > 256:
+            flash("Response Too Long!")
+            return redirect('/')
         full_name = request.form['name']
         dojo_location = request.form['Dojo_Location']
         fav_lan = request.form['Favorite_Language']
         comment = request.form['optional_comment']
+    
     return render_template('results.html', full_name = full_name, dojo_location = dojo_location, fav_lan = fav_lan, comment = comment)
         
         
